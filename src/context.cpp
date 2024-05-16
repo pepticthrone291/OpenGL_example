5408bce1ebe1ee204849060b211353c39354da08
pepticthrone291 <intercorers@gmail.com>
2024-05-16 18:02:18 +0900

    texture2 두 개 이상의 텍스처

--- src/context.cpp
+++ src/context.cpp
@@ -46,15 +46,27 @@ bool Context::Init()
 
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
 
-    // auto image = Image::Load("./image/container.jpg");
-    // if (!image) 
-    //     return false;
-    // SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+    auto image = Image::Load("./image/container.jpg");
+    if (!image) 
+        return false;
+    SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
-    auto image = Image::Create(512, 512);
-    image->SetCheckImage(16, 16);
+    // auto image = Image::Create(512, 512);
+    // image->SetCheckImage(16, 16);
 
     m_texture = Texture::CreateFromImage(image.get());  // get : raw pointer
+
+    auto image2 = Image::Load("./image/awesomeface.png");
+    m_texture2 = Texture::CreateFromImage(image2.get());
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_texture->Get());
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, m_texture2->Get());
+
+    m_program->Use();
+    glUniform1i(glGetUniformLocation(m_program->Get(), "tex"), 0);
+    glUniform1i(glGetUniformLocation(m_program->Get(), "tex2"), 1);
 
     return true;
 }
